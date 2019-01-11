34f28adac9e44b05f303cb10e6ffcc97278c5da6
Harold Espinoza <haroldesptru@gmail.com>
2019-01-10 22:18:19 -0600

    Array.prototype.sortByString(ascendent=true): función para ordernar únicamente las cadenas de text dentro del array, el parámetro ascendent indica si los valores se van a ordenar de manera ascendente o descendente. Si se encuentra algún otro tipo de dato que no sea un string éste es reasignado al final del array. Si el array no contiene ningún string, un mesaje de error es imprimido en consola

--- arrayExtensions.js
+++ arrayExtensions.js
@@ -6,6 +6,7 @@
 let data = [2,8,4,"a","d","z",5,7,1],
     data2 = ["b", "c", "f"];
 
+// Sort all numbers into array, all data that is't number is pushing to end
 Array.prototype.sortByNumber = function(ascendent=true){
   let arrayToSort = [],
       arrayToNotSort = [],
@@ -36,5 +37,41 @@ Array.prototype.sortByNumber = function(ascendent=true){
 };
 
 
+// Sort all strings into array, all data that is't string is puching to end
+Array.prototype.sortByString = function(ascendent=true){
+  let arrayToSort = [],
+    arrayToNotSort = [],
+    typeSorted = `a.toLocaleLowerCase() ${(ascendent) ? '>' : '<'} b.toLocaleLowerCase()`,
+    cont = 0;
+
+  // Detect all numbers that can be sorted, and other data that can't
+  for(let value of this){
+    if(typeof value !== "string"){
+      arrayToNotSort.push(value);
+    }else{
+      arrayToSort.push(value);
+    }
+  }
+
+  // Verify if number array is not empty
+  if (arrayToSort.length === 0){
+    console.error("The array not contains strings to sort...");
+    return undefined;
+  }
+
+  let tempArray = arrayToSort.sort( (a,b) => eval(typeSorted) ).concat(arrayToNotSort);
+
+  // Saving the array result
+  for(let value of tempArray){
+    this[cont++] = value;
+  }
+};
+
+
+// Sort all numbers into array
 data.sortByNumber();
 data2.sortByNumber();
+
+// Sort all strings into array
+data.sortByString(true);
+data2.sortByString();
